2ac9ad1b73576cb67f88fee44b9408fb2a730b4d
ritamak <ritapmak@gmail.com>
2021-07-21 11:04:54 +0100

    changed user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,12 +1,37 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
     type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
+    unique: true,
+    required: true,
   },
-  password: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = model("User", userSchema);
