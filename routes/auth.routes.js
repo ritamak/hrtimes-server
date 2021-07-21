8b60b2040fe422ef38195c66f69fda2ae41d6c63
ritamak <ritapmak@gmail.com>
2021-07-21 14:18:23 +0100

    sign up post request working

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -12,12 +12,12 @@ router.post("/signup", (req, res) => {
     country,
     city,
     image,
-    password,
+    passwordHash,
   } = req.body;
   if (
     !username ||
     !email ||
-    !password ||
+    !passwordHash ||
     !firstName ||
     !lastName ||
     !city ||
@@ -40,7 +40,7 @@ router.post("/signup", (req, res) => {
   const myPassRegex = new RegExp(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
   );
-  if (!myPassRegex.test(password)) {
+  if (!myPassRegex.test(passwordHash)) {
     res.status(500).json({
       errorMessage:
         "Password needs to have 8 characters, a number and an Uppercase alphabet",
@@ -49,7 +49,7 @@ router.post("/signup", (req, res) => {
   }
 
   let salt = bcrypt.genSaltSync(10);
-  let hash = bcrypt.hashSync(password, salt);
+  let hash = bcrypt.hashSync(passwordHash, salt);
   UserModel.create({
     username,
     firstName,
@@ -145,7 +145,7 @@ const isLoggedIn = (req, res, next) => {
   }
 };
 
-router.get("/profile", isLoggedIn, (req, res, next) => {
+router.get("/profile", (req, res, next) => {
   res.status(200).json(req.session.loggedInUser);
 });
 
