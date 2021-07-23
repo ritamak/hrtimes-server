5666a281565050e1334231fd544f05c0e83415f4
ritamak <ritapmak@gmail.com>
2021-07-23 10:58:52 +0100

    create article working

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -2,6 +2,11 @@ const router = require("express").Router();
 const axios = require("axios");
 const ArticleModel = require("../models/Article.model");
 
+router.get("/create", (req, res, next) => {
+  console.log("router get for create article works");
+  res.status(200).json(req.session.loggedInUser);
+});
+
 router.post("/create", (req, res, next) => {
   const { section, subsection, title, body, created_date, author } = req.body;
   console.log(req.body);
