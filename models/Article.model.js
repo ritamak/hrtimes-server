ae2cf1aaa0663f8cddcb5e92133db9a32777e878
ritamak <ritapmak@gmail.com>
2021-07-26 11:27:44 +0100

    fixed author

--- models/Article.model.js
+++ models/Article.model.js
@@ -15,7 +15,6 @@ const articleSchema = new Schema({
   },
   body: {
     type: String,
-    unique: true,
     required: true,
   },
   created_date: {
@@ -24,12 +23,14 @@ const articleSchema = new Schema({
   },
   author: {
     type: Schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
-  comments: [{
-    type: Schema.Types.ObjectId,
-    ref: "Comment"
-  }],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
 });
 
 const Article = model("Article", articleSchema);
