77aaec839c801584f57213e4f1daec38c80a0b44
ritamak <ritapmak@gmail.com>
2021-07-28 12:02:49 +0100

    nothing changed

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const UserModel = require("../models/User.model");
-const ArticleModel = require('../models/Article.model');
-const CommentModel = require('../models/Comment.model');
+const ArticleModel = require("../models/Article.model");
+const CommentModel = require("../models/Comment.model");
 
 router.get("/users/:id", (req, res, next) => {
   const { id } = req.params;
   const { _id } = req.session.loggedInUser;
 
   UserModel.findById(id)
-    .populate('comments')
-    .populate('articles')
-    .populate('following')
+    .populate("comments")
+    .populate("articles")
+    .populate("following")
     .then((user) => {
       console.log(user);
       res.status(200).json(user);
     })
     .catch((err) => {
-        res.status(500).json({
+      res.status(500).json({
         error: "Something went wrong",
         message: err,
       });
@@ -27,13 +27,15 @@ router.get("/users/:id", (req, res, next) => {
 router.post("/users/:id", (req, res, next) => {
   const { id } = req.params;
   const { _id } = req.session.loggedInUser;
-
-  UserModel.findByIdAndUpdate((_id), {$push: {following: id}})
+  console.log("this is loggedInUser", req.session.loggedInUser);
+  console.log("this is the user to follow", req.params);
+  UserModel.findByIdAndUpdate(_id, { $push: { following: id } })
     .then((user) => {
       res.status(200).json(user);
+      console.log("it works!");
     })
     .catch((err) => {
-        console.log(err);
+      console.log(err);
     });
 });
 
